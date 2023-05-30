5c8576897535136a0f1ef6c1fdd752adf36590c7
Александр <tarasov.slavas2002@gmail.com>
2023-05-30 17:34:10 +0000

    фикс вебхука

--- project/telegram_bot/views.py
+++ project/telegram_bot/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 
 from rest_framework.views import APIView
+from loguru import logger
 
 from .loader import BOT
 
@@ -9,14 +10,16 @@ class TelegramWebhook(APIView):
     def post(self, request):
         try:
             # TODO Добавить статус "Печатает"
-            print(request.data)
-            user_id = request.data.get("id").get("from").get("id")
+            logger.info(request.data)
+            for key, value in request.data.items():
+                logger.info(key, value, "FDLS")
+            user_id = request.data.get("message").get("from").get("id")
             message = request.data.get("message").get("text")
             TelegramWebhook._handle_message(user_id=user_id, message=message)
-            return JsonResponse({"success": True})
+            return JsonResponse({"success": request.data})
         except Exception as _exec:
-            print(_exec)
-            return JsonResponse({"Error": "Some error occured."})
+            logger.error(f"{_exec}")
+            return JsonResponse({"Error": "Some error occurmessageed."})
 
     @classmethod
     def _handle_message(cls, user_id: int, message: str):
