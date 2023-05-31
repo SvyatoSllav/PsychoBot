931a6a6261ac62e316d5c0ddf8c8d006545f0260
Александр <tarasov.slavas2002@gmail.com>
2023-05-31 20:48:21 +0000

    Убрал ненужный путь

--- project/core/urls.py
+++ project/core/urls.py
@@ -6,14 +6,13 @@ from . import settings
 
 from telegram_bot.views import TelegramWebhook
 
-from tasks.views import get_status, home, run_task
+from tasks.views import get_status, run_task
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("webhook/", TelegramWebhook.as_view(), name="tg_webhook"),
     path("tasks/<task_id>/", get_status, name="get_status"),
     path("tasks/", run_task, name="run_task"),
-    path("", home, name="home"),
 ]
 
 if settings.DEBUG:
