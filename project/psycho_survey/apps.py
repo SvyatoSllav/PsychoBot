cf3c532731300d4f4d12047c343461d8c72de671
Александр <tarasov.slavas2002@gmail.com>
2023-06-01 20:13:37 +0000

    Обновил название приложения для админки

--- project/psycho_survey/apps.py
+++ project/psycho_survey/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class PsychoSurveyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'psycho_survey'
-    verbose_name = "Тесты, задачи, ответы"
+    verbose_name = "Тесты, задачи, ответы, отзывы"
