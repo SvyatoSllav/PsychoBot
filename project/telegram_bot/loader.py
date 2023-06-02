dca28c47b4ad710ab50f161a72f3c1b1fa1091cc
Александр <tarasov.slavas2002@gmail.com>
2023-06-02 22:59:00 +0000

    Поменял токен бота

--- project/telegram_bot/loader.py
+++ project/telegram_bot/loader.py
@@ -1,6 +1,6 @@
 import telebot
 
+from core.settings import BOT_TOKEN
 
-# Test token. Already changed
-TOKEN = '5129570063:AAEcfTAAGA0CTV70Voj_O2zOO5D0s85kiak'
-BOT = telebot.TeleBot(TOKEN)
+
+BOT = telebot.TeleBot(BOT_TOKEN)
