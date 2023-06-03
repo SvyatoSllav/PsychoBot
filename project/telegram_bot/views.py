b8a24d31275437f08beb0e36fc1f4911878d82eb
notStonks <kuzewich.a@gmail.com>
2023-06-03 20:21:19 +0300

    небольшие исправления

--- project/telegram_bot/views.py
+++ project/telegram_bot/views.py
@@ -147,30 +147,6 @@ class TelegramWebhook(ValidatorsMixin, MessageHandlers, APIView):
             logger.error(f"{_exec}")
             return JsonResponse({"Error": "error during payment"})
 
-    @staticmethod
-    def confirmed_payment(userid: str):
-        """
-        Посылает пользователю сообщение, что оплата прошла успешно и меняет статус в БД
-        """
-        user = TelegramUser.objects.get(id=userid)
-        user.bought_course = True
-        user.save()
-
-        BOT.send_message(chat_id=user.user_id, text=f"Оплата прошла успешно")
-
-    @staticmethod
-    def rejected_payment(userid: str):
-        """
-        Посылает пользователю сообщение в случае непрошедшей оплаты и отправляет новую ссылку на повторную оплату
-        """
-        user = TelegramUser.objects.get(id=userid)
-        payment = TinkoffSimplePayment(terminal_id="1685039843752DEMO", password="jcw9vwrfgqx8fn0b")
-        payment_result = payment.init(userid, "100", sign_request=True,
-                                      notificationURL="https://8082-193-242-207-246.ngrok-free.app/payhook/",
-                                      data={"Phone": "+79999999999"})
-        payment_url = payment_result['PaymentURL']
-        BOT.send_message(chat_id=user.user_id, text=f"Ошибка при оплате. Повторите попытку\n{payment_url}")
-
     @classmethod
     def _proccess_undefined_message(cls, user_id: int, message: str):
         """
--- project/tinkoff/views.py
+++ project/tinkoff/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from loguru import logger
 
+from telegram_bot.Tinkoff import TinkoffSimplePayment
+from telegram_bot.loader import BOT
+from telegram_bot.models import TelegramUser
 from telegram_bot.views import TelegramWebhook
 
 
@@ -12,10 +15,42 @@ class TinkoffWebhook(APIView):
             logger.info(request.data)
             status = request.data["Status"]
             if status == "CONFIRMED":
-                TelegramWebhook.confirmed_payment(request.data["OrderId"])
+                TinkoffWebhook._confirmed_payment(request.data["OrderId"])
             if status == "REJECTED":
-                TelegramWebhook.rejected_payment(request.data["OrderId"])
+                TinkoffWebhook._rejected_payment(request.data["OrderId"])
             return JsonResponse({"success": request.data})
+        except Exception as _exec:
+            logger.error(f"{_exec}")
+            return JsonResponse({"Error": "Some error occured."})
+
+
+    @classmethod
+    def _confirmed_payment(cls, pk: str):
+        """
+        Посылает пользователю сообщение, что оплата прошла успешно и меняет статус в БД
+        """
+        try:
+            user = TelegramWebhook._get_object_or_none(TelegramUser, id=id)
+            user.bought_course = True
+            user.save()
+            BOT.send_message(chat_id=user.user_id, text="Оплата прошла успешно")
+        except Exception as _exec:
+            logger.error(f"{_exec}")
+            return JsonResponse({"Error": "Some error occured."})
+
+    @classmethod
+    def _rejected_payment(cls, id: str):
+        """
+        Посылает пользователю сообщение в случае непрошедшей оплаты и отправляет новую ссылку на повторную оплату
+        """
+        try:
+            user = TelegramWebhook._get_object_or_none(TelegramUser, id=id)
+            payment = TinkoffSimplePayment(terminal_id="1685039843752DEMO", password="jcw9vwrfgqx8fn0b")
+            payment_result = payment.init(userid, "100", sign_request=True,
+                                          notificationURL="https://8082-193-242-207-246.ngrok-free.app/payhook/",
+                                          data={"Phone": "+79999999999"})
+            payment_url = payment_result['PaymentURL']
+            BOT.send_message(chat_id=user.user_id, text=f"Ошибка при оплате. Повторите попытку\n{payment_url}")
         except Exception as _exec:
             logger.error(f"{_exec}")
             return JsonResponse({"Error": "Some error occured."})
